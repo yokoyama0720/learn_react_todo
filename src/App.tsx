b7f71bac23fdebc5150dd850f7ad4247621aeb67
Yu Yokoyama <21t2162a@shinshu-u.ac.jp>
2024-11-05 01:15:16 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Todos {
+  id: number;
+  task: string;
+  completed: boolean;
 }
 
-export default App
+export default function MyApp() {
+  // todosを管理するためのstateを定義
+  const [todos, setTodos] = useState<Todos[]>([]);
+  // idを管理するためのstateを定義
+  const [id, setId] =useState<number>(0);
+  // 入力されたtaskを管理するためのstateを定義
+  const [task, setTask] = useState<string>("");
+  // 追加ボタンを押したときの挙動を定義
+  const onClickAdd = () => {
+    // id をインクリメント
+    setId(id + 1);
+    const newTodos: Todos = {
+      id: id,
+      task,
+      completed: false,
+    }
+    // 新しいtodoを追加
+    setTodos([...todos, newTodos])
+    // 入力フィールドをクリア
+    setTask("");
+  }
+
+  return (
+    <div>
+      <h1>Todoリスト</h1>
+      <input
+        type="text"
+        name="task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="タスクを入力してください"
+      />
+      {/* onclickの{}にボタンを押したときの挙動を定義した関数を指定する */}
+      <button onClick={onClickAdd}>追加</button>
+      <ul>
+        {todos.map((todo) => (
+          <li>{todo.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
